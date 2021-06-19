fe826f79b638c35e480f99c55bb810cad3b2a446
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-19 22:37:22 +0300

    fix: убирает лишнюю оболочку

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from 'react'
 import { DefaultLayout } from '../../layouts/Default'
-import { PageContainer } from '../../components/PageContainer'
 import { ButtonRouter } from '../../components/ButtonRouter'
 import { ReactComponent as NotFoundSvg } from '../../assets/settings-not-found.svg'
 import './style.scss'
@@ -8,25 +7,23 @@ import './style.scss'
 export const MainPage: FunctionComponent = (props) => {
   return (
     <DefaultLayout>
-      <PageContainer>
-        <div className="no-settings">
-          <NotFoundSvg />
+      <div className="no-settings">
+        <NotFoundSvg />
 
-          <p className="no-settings__text">
-            Configure repository connection
-            <br /> and synchronization settings
-          </p>
+        <p className="no-settings__text">
+          Configure repository connection
+          <br /> and synchronization settings
+        </p>
 
-          <ButtonRouter
-            path="/settings"
-            buttonProps={{
-              btnStyle: 'accent',
-            }}
-          >
-            Open settings
-          </ButtonRouter>
-        </div>
-      </PageContainer>
+        <ButtonRouter
+          path="/settings"
+          buttonProps={{
+            btnStyle: 'accent',
+          }}
+        >
+          Open settings
+        </ButtonRouter>
+      </div>
     </DefaultLayout>
   )
 }
