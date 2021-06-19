1686a917ac9223bab143e04ccb75fea085c23e88
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-19 22:13:52 +0300

    feat: страница для отображения лога билда

--- src/pages/Build/index.tsx
+++ src/pages/Build/index.tsx
@@ -1,9 +1,60 @@
 import { FunctionComponent } from 'react'
 import { DefaultLayout } from '../../layouts/Default'
 import { useParams } from 'react-router-dom'
+import { BuildCard } from '../../components/BuildCard'
+import { Button } from '../../components/Button'
+import { ReactComponent as ReloadSvg } from '../../assets/reload.svg'
+import { LogsField } from '../../components/LogsField'
+import './style.scss'
+
+import { log } from '../../testlog'
 
 export const BuildPage: FunctionComponent = (props) => {
   const params = useParams<{ buildId: string }>()
 
-  return <DefaultLayout>BuildPage {params.buildId}</DefaultLayout>
+  // TODO: не забыть
+  const oneHour = 3_600_000
+  const tenMins = 600_000
+  const testDuration = oneHour * 3 + tenMins * 2.5 // 3 h 25 min
+  const testDate = '2021-04-17T10:20:30.931Z'
+
+  const settingsButton = (
+    <>
+      <div className="rebuild-button">
+        <Button
+          size="xs"
+          nativeAttrs={{
+            title: 'Rebuild',
+          }}
+        >
+          <ReloadSvg />
+        </Button>
+      </div>
+      <div className="rebuild-button-desktop">
+        <Button size="sm" prepend={<ReloadSvg />}>
+          Rebuild
+        </Button>
+      </div>
+    </>
+  )
+
+  return (
+    <DefaultLayout
+      title="philip1967/my-awesome-repo"
+      addButtons={settingsButton}
+    >
+      <BuildCard
+        author="Philip Kirkorov"
+        commitHash="34e4kf8j384d2w3dijf9"
+        duration={testDuration}
+        number="1"
+        msg="add documentation for postgres scaler"
+        mainBranch="main"
+        status="success"
+        startDate={testDate}
+      />
+
+      <LogsField className="log" log={log} />
+    </DefaultLayout>
+  )
 }
---
+++ src/testlog.ts
@@ -0,0 +1,103 @@
+export const log = `
+> zaimfast-app@ build /home/ikosh/dev/zaimfast-nuxt
+> nuxt build
+
+ℹ ⚙ Running in standard environment
+ℹ Production build
+ℹ Bundling for server and client side
+ℹ Target: server
+✔ Builder initialized
+✔ Nuxt files generated
+
+ WARN  Browserslist: caniuse-lite is outdated. Please run:
+npx browserslist@latest --update-db
+
+Why you should do it regularly:
+https://github.com/browserslist/browserslist#browsers-data-updating
+
+ℹ Compiling Client
+✔ Client: Compiled successfully in 18.93s
+ℹ Compiling Modern
+✔ Modern: Compiled successfully in 14.78s
+ℹ Compiling Server
+✔ Server: Compiled successfully in 7.47s
+
+Hash: [1m27c22f6eb2ff5c29c9e4[39m[22m
+Version: webpack [1m4.44.2[39m[22m
+Time: [1m18928[39m[22mms
+Built at: 06/19/2021 [1m9:06:16 PM[39m[22m
+                               [1mAsset[39m[22m       [1mSize[39m[22m  [1mChunks[39m[22m  [1m[39m[22m                       [1m[39m[22m[1mChunk Names[39m[22m
+      [1m[32m../server/client.manifest.json[39m[22m   48.7 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                          [1m[32m0dcb125.js[39m[22m   11.7 KiB       [1m2[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/about/index
+                          [1m[32m21bb379.js[39m[22m    102 KiB      [1m12[39m[22m  [1m[32m[emitted] [immutable][39m[22m  vendors/pages/index
+                          [1m[32m3063538.js[39m[22m   15.3 KiB       [1m7[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/mfo/_item
+                          [1m[32m42cd08c.js[39m[22m    7.8 KiB       [1m5[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/articles/index
+                          [1m[32m470c64d.js[39m[22m   60.7 KiB       [1m9[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/zajmy/_slug
+                          [1m[32m618ca04.js[39m[22m   2.41 KiB      [1m10[39m[22m  [1m[32m[emitted] [immutable][39m[22m  runtime
+                          [1m[32m693111e.js[39m[22m  266 bytes       [1m8[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/zajmy
+[1m[32m6f54bb76b060eade63f1397a1b026106.svg[39m[22m   25.1 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                          [1m[32m7598529.js[39m[22m   26.5 KiB       [1m3[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/about/privacy-policy
+                          [1m[32m78937ab.js[39m[22m    113 KiB      [1m11[39m[22m  [1m[32m[emitted] [immutable][39m[22m  vendors/app
+                          [1m[32m7b6cac3.js[39m[22m   4.43 KiB      [1m14[39m[22m  [1m[32m[emitted] [immutable][39m[22m
+                          [1m[32m8467296.js[39m[22m   6.25 KiB       [1m4[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/articles/_slug
+[1m[32m89feddefc9118c3514e4a4152444c9c5.svg[39m[22m   11.6 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                            [1m[32mLICENSES[39m[22m  791 bytes        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                          [1m[32maf72097.js[39m[22m    182 KiB       [1m0[39m[22m  [1m[32m[emitted] [immutable][39m[22m  app
+                          [1m[32md4cb9a5.js[39m[22m    134 KiB      [1m13[39m[22m  [1m[32m[emitted] [immutable][39m[22m  vendors/pages/mfo/_item
+                          [1m[32me65b148.js[39m[22m    189 KiB       [1m1[39m[22m  [1m[32m[emitted] [immutable][39m[22m  commons/app
+[1m[32mf4fb2feb506579904b85c1fd691370fd.svg[39m[22m   3.98 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                          [1m[32mfa4d431.js[39m[22m   28.1 KiB       [1m6[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/index
+          [1m[32mimg/bck0-dfgdr.8bd4958.png[39m[22m   10.7 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                [1m[32mimg/logo.355e6bd.svg[39m[22m   4.29 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+ + 2 hidden assets
+Entrypoint [1mapp[39m[22m = [1m[32m618ca04.js[39m[22m [1m[32me65b148.js[39m[22m [1m[32m78937ab.js[39m[22m [1m[32maf72097.js[39m[22m
+
+Hash: [1m5bbd9eb3634e3a66d809[39m[22m
+Version: webpack [1m4.44.2[39m[22m
+Time: [1m14783[39m[22mms
+Built at: 06/19/2021 [1m9:06:30 PM[39m[22m
+                               [1mAsset[39m[22m       [1mSize[39m[22m  [1mChunks[39m[22m  [1m[39m[22m                       [1m[39m[22m[1mChunk Names[39m[22m
+      [1m[32m../server/modern.manifest.json[39m[22m   46.8 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                   [1m[32m0351413.modern.js[39m[22m   26.6 KiB       [1m6[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/index
+                   [1m[32m250366b.modern.js[39m[22m    163 KiB       [1m1[39m[22m  [1m[32m[emitted] [immutable][39m[22m  commons/app
+                   [1m[32m3c7bd11.modern.js[39m[22m    102 KiB      [1m12[39m[22m  [1m[32m[emitted] [immutable][39m[22m  vendors/pages/index
+                   [1m[32m4c29c5d.modern.js[39m[22m   7.46 KiB       [1m5[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/articles/index
+                   [1m[32m5fb0baa.modern.js[39m[22m   14.8 KiB       [1m7[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/mfo/_item
+                   [1m[32m5fb64eb.modern.js[39m[22m   4.43 KiB      [1m14[39m[22m  [1m[32m[emitted] [immutable][39m[22m
+[1m[32m6f54bb76b060eade63f1397a1b026106.svg[39m[22m   25.1 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                   [1m[32m7d06547.modern.js[39m[22m   5.95 KiB       [1m4[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/articles/_slug
+                   [1m[32m8058917.modern.js[39m[22m   58.6 KiB       [1m9[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/zajmy/_slug
+[1m[32m89feddefc9118c3514e4a4152444c9c5.svg[39m[22m   11.6 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                   [1m[32m8a7aef6.modern.js[39m[22m    113 KiB      [1m11[39m[22m  [1m[32m[emitted] [immutable][39m[22m  vendors/app
+                            [1m[32mLICENSES[39m[22m  791 bytes        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                   [1m[32ma4c52bd.modern.js[39m[22m    162 KiB       [1m0[39m[22m  [1m[32m[emitted] [immutable][39m[22m  app
+                   [1m[32mb48ceef.modern.js[39m[22m   11.6 KiB       [1m2[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/about/index
+                   [1m[32mc412a60.modern.js[39m[22m   26.4 KiB       [1m3[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/about/privacy-policy
+                   [1m[32mcca1e4f.modern.js[39m[22m    134 KiB      [1m13[39m[22m  [1m[32m[emitted] [immutable][39m[22m  vendors/pages/mfo/_item
+[1m[32mf4fb2feb506579904b85c1fd691370fd.svg[39m[22m   3.98 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                   [1m[32mf813184.modern.js[39m[22m   2.42 KiB      [1m10[39m[22m  [1m[32m[emitted] [immutable][39m[22m  runtime
+                   [1m[32mfeba429.modern.js[39m[22m  266 bytes       [1m8[39m[22m  [1m[32m[emitted] [immutable][39m[22m  pages/zajmy
+          [1m[32mimg/bck0-dfgdr.8bd4958.png[39m[22m   10.7 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+                [1m[32mimg/logo.355e6bd.svg[39m[22m   4.29 KiB        [1m[39m[22m  [1m[32m[emitted][39m[22m
+ + 2 hidden assets
+Entrypoint [1mapp[39m[22m = [1m[32mf813184.modern.js[39m[22m [1m[32m250366b.modern.js[39m[22m [1m[32m8a7aef6.modern.js[39m[22m [1m[32ma4c52bd.modern.js[39m[22m
+
+Hash: [1m12551030aadce3dfd7c5[39m[22m
+Version: webpack [1m4.44.2[39m[22m
+Time: [1m7470[39m[22mms
+Built at: 06/19/2021 [1m9:06:38 PM[39m[22m
+                        [1mAsset[39m[22m       [1mSize[39m[22m  [1mChunks[39m[22m  [1m[39m[22m           [1m[39m[22m[1mChunk Names[39m[22m
+         [1m[32mpages/about/index.js[39m[22m   18.9 KiB       [1m1[39m[22m  [1m[32m[emitted][39m[22m  pages/about/index
+[1m[32mpages/about/privacy-policy.js[39m[22m   38.1 KiB       [1m2[39m[22m  [1m[32m[emitted][39m[22m  pages/about/privacy-policy
+      [1m[32mpages/articles/_slug.js[39m[22m   20.2 KiB       [1m3[39m[22m  [1m[32m[emitted][39m[22m  pages/articles/_slug
+      [1m[32mpages/articles/index.js[39m[22m   24.7 KiB       [1m4[39m[22m  [1m[32m[emitted][39m[22m  pages/articles/index
+               [1m[32mpages/index.js[39m[22m   78.1 KiB       [1m5[39m[22m  [1m[32m[emitted][39m[22m  pages/index
+           [1m[32mpages/mfo/_item.js[39m[22m   34.2 KiB       [1m6[39m[22m  [1m[32m[emitted][39m[22m  pages/mfo/_item
+               [1m[32mpages/zajmy.js[39m[22m    1.1 KiB       [1m7[39m[22m  [1m[32m[emitted][39m[22m  pages/zajmy
+         [1m[32mpages/zajmy/_slug.js[39m[22m    145 KiB       [1m8[39m[22m  [1m[32m[emitted][39m[22m  pages/zajmy/_slug
+                    [1m[32mserver.js[39m[22m    544 KiB       [1m0[39m[22m  [1m[32m[emitted][39m[22m  app
+         [1m[32mserver.manifest.json[39m[22m  991 bytes        [1m[39m[22m  [1m[32m[emitted][39m[22m
+ + 9 hidden assets
+Entrypoint [1mapp[39m[22m = [1m[32mserver.js[39m[22m [1m[32mserver.js.map[39m[22m
+ℹ Ready to run nuxt start
+`
